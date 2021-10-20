2f514b6b49d51de745566d72df06a91552f0d4eb
Croix Kimmel <croixkimmel@gmail.com>
2021-10-20 09:26:58 -0600

    Troubleshoot board, board_spec.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,36 +1,62 @@
+require'./lib/cell'
+require './lib/ship'
+
 class Board
 
-  attr_reader
+  attr_reader :cells
 
   def initialize
+    @cell_1 = Cell.new('A1')
+    @cell_2 = Cell.new('A2')
+    @cell_3 = Cell.new('A3')
+    @cell_4 = Cell.new('A4')
+    @cell_5 = Cell.new('B1')
+    @cell_6 = Cell.new('B2')
+    @cell_7 = Cell.new('B3')
+    @cell_8 = Cell.new('B4')
+    @cell_9 = Cell.new('C1')
+    @cell_10 = Cell.new('C2')
+    @cell_11 = Cell.new('C3')
+    @cell_12 = Cell.new('C4')
+    @cell_13 = Cell.new('D1')
+    @cell_14 = Cell.new('D2')
+    @cell_15 = Cell.new('D3')
+    @cell_16 = Cell.new('D4')
+
 
     @cells = {
-      "A1" => cell_1 = Cell.new('A1'),
-      "A2" => cell_2 = Cell.new("A2"),
-      "A3" => cell_3 = Cell.new("A3"),
-      "A4" => cell_4 = Cell.new("A4"),
-      "B1" => cell_5 = Cell.new("B1"),
-      "B2" => cell_6 = Cell.new("B2"),
-      "B3" => cell_7 = Cell.new("B3"),
-      "B4" => cell_8 = Cell.new("B4"),
-      "C1" => cell_9 = Cell.new("C1"),
-      "C2" => cell_10 = Cell.new("C2"),
-      "C3" => cell_11 = Cell.new("C3"),
-      "C4" => cell_12 = Cell.new("C4"),
-      "D1" => cell_13 = Cell.new("D1"),
-      "D2" => cell_14 = Cell.new("D2"),
-      "D3" => cell_15 = Cell.new("D3"),
-      "D4" => cell_16 = Cell.new("D4")
+      "A1" => @cell_1,
+      "A2" => @cell_2,
+      "A3" => @cell_3,
+      "A4" => @cell_4,
+      "B1" => @cell_5,
+      "B2" => @cell_6,
+      "B3" => @cell_7,
+      "B4" => @cell_8,
+      "C1" => @cell_9,
+      "C2" => @cell_10,
+      "C3" => @cell_11,
+      "C4" => @cell_12,
+      "D1" => @cell_13,
+      "D2" => @cell_14,
+      "D3" => @cell_15,
+      "D4" => @cell_16
     }
 
   end
 
+
+  # doesn't seem to be failing E - why?
+  # also needs to fail A22 - currently just pulls 2, not 22, doesn't fail
   def valid_coordinate?(cell_coordinates)
-    cell_coordinates = cell_coordinates.split!(//)
+    cell_coordinates = cell_coordinates.split(//)
     letter = cell_coordinates[0]
-    number = cell_coordinates[1]
+    number = cell_coordinates[1].to_i
+
+    # require "pry"; binding.pry
+
     # complete this
-    if ("A".."Z").include?(letter) && (1..4).include?(number)
+    if ("A".."D").include?(letter) && (1 <= number) && (4 >= number)
       return true
     else
       return false
@@ -39,33 +65,33 @@ class Board
 
 
 
-  def valid_placement?(ship_name, ship_cells)
-    # number of coordinates
-    letters = []
-    numbers = []
-
-    ship_cells.each do |cell|
-      coordinates = cell.split(//)
-      letters << cell_coordinates[0]
-      numbers << cell_coordinates[1]
-    end
-
-    letters_consecutive = letters.sort
-    numbers_consecutive = numbers.sort
-
-    if ship_length != ship_cell.length
-      return false
-    # consecutive coordinates
-  elsif letters != letters_consecutive || numbers != numbers_consecutive
-      return false
-    # diagonal check - do something along the lines of last element minus first
-      # element of one can't equal that of the other (one has to be 0)
-    elsif #### ADD CODE HERE
-      return false
-    else
-      return true
-    end
-  end
+  # def valid_placement?(ship_name, ship_cells)
+  #   # number of coordinates
+  #   letters = []
+  #   numbers = []
+  #
+  #   ship_cells.each do |cell|
+  #     coordinates = cell.split(//)
+  #     letters << cell_coordinates[0]
+  #     numbers << cell_coordinates[1]
+  #   end
+  #
+  #   letters_consecutive = letters.sort
+  #   numbers_consecutive = numbers.sort
+  #
+  #   if ship_length != ship_cell.length
+  #     return false
+  #   # consecutive coordinates
+  # elsif letters != letters_consecutive || numbers != numbers_consecutive
+  #     return false
+  #   # diagonal check - do something along the lines of last element minus first
+  #     # element of one can't equal that of the other (one has to be 0)
+  #   elsif #### ADD CODE HERE
+  #     return false
+  #   else
+  #     return true
+  #   end
+  # end
 
   def place(ship_name, ship_coordinates)
     # modify each cell specified to contain ship ship_name
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,7 +5,7 @@ require './lib/ship'
 RSpec.describe Board do
     # will want to use before each to keep file shorter
   before (:each) do
-    board = Board.new()
+    @board = Board.new()
   end
 
   it 'is a valid coordinate - A1' do
@@ -30,46 +30,46 @@ RSpec.describe Board do
   end
 
 
-  it 'is a valid placement - length' do
-    expect(@board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
-  end
-
-
-  it 'is a valid placement - length' do
-    expect(@board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
-  end
-
-  it 'is a valid placement - consecutive' do
-    expect(@board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
-  end
-
-  it 'is a valid placement - consecutive' do
-    expect(@board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
-  end
-
-  it 'is a valid placement - consecutive' do
-    expect(@board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
-  end
-
-  it 'is a valid placement - consecutive' do
-    expect(@board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
-  end
-
-  it 'is a valid placement - diagonal' do
-    expect(@board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
-  end
-
-  it 'is a valid placement - diagonal' do
-    expect(@board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
-  end
-
-  it 'is a valid placement - final check' do
-    expect(@board.valid_placement?(submarine, ["A1", "A2"])).to eq(false)
-  end
-
-  it 'is a valid placement - final check' do
-    expect(@board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(false)
-  end
+  # it 'is a valid placement - length' do
+  #   expect(@board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+  # end
+  #
+  #
+  # it 'is a valid placement - length' do
+  #   expect(@board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  # end
+  #
+  # it 'is a valid placement - consecutive' do
+  #   expect(@board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+  # end
+  #
+  # it 'is a valid placement - consecutive' do
+  #   expect(@board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+  # end
+  #
+  # it 'is a valid placement - consecutive' do
+  #   expect(@board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+  # end
+  #
+  # it 'is a valid placement - consecutive' do
+  #   expect(@board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+  # end
+  #
+  # it 'is a valid placement - diagonal' do
+  #   expect(@board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+  # end
+  #
+  # it 'is a valid placement - diagonal' do
+  #   expect(@board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+  # end
+  #
+  # it 'is a valid placement - final check' do
+  #   expect(@board.valid_placement?(submarine, ["A1", "A2"])).to eq(false)
+  # end
+  #
+  # it 'is a valid placement - final check' do
+  #   expect(@board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(false)
+  # end
 
   ##### PICK UP HERE WITH TESTS FOR 'PLACING SHIPS' SECTION
 
