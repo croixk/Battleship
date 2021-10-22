482fa7b6b4fd38eb0474286209afe9f2b3676c62
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-22 15:24:33 -0600

    Add 2 additional place method tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -113,26 +113,26 @@ RSpec.describe Board do
     cell_3 = board.cells["A3"]
     expect(board.cells["A1"].ship).to eq(cruiser)
   end
-  #
-  # it 'place ship A2 - does ship associated with cell equal ship' do
-  #   board = Board.new
-  #   cruiser = Ship.new("Cruiser", 3)
-  #   board.place(cruiser, ["A1", "A2", "A3"])
-  #   cell_1 = board.cells["A1"]
-  #   cell_2 = board.cells["A2"]
-  #   cell_3 = board.cells["A3"]
-  #   expect(@board.cells["A1"].ship).to eq(cruiser)
-  # end
-  #
-  # it 'place ship A3 - does ship associated with cell equal ship' do
-  #   board = Board.new
-  #   cruiser = Ship.new("Cruiser", 3)
-  #   board.place(cruiser, ["A1", "A2", "A3"])
-  #   cell_1 = board.cells["A1"]
-  #   cell_2 = board.cells["A2"]
-  #   cell_3 = board.cells["A3"]
-  #   expect(@board.cells["A1"].ship).to eq(cruiser)
-  # end
+
+  it 'place ship A2 - does ship associated with cell equal ship' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+    expect(board.cells["A1"].ship).to eq(cruiser)
+  end
+
+  it 'place ship A3 - does ship associated with cell equal ship' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+    expect(board.cells["A1"].ship).to eq(cruiser)
+  end
 
 
 
