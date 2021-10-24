6db494040b5482e24aad536b48745a12a2a9dc83
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-24 13:29:18 -0600

    Add random sub placement method test

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -10,18 +10,20 @@ RSpec.describe Computer do
   end
 
   it 'can select random valid placement (cruiser)' do
-
     board = Board.new()
     cruiser = Ship.new("Cruiser", 3)
     computer = Computer.new()
 
     expect(board.valid_placement?(cruiser, computer.random_cruiser_position)).to eq true
-
   end
 
+  it 'can select random valid placement (submarine)' do
+    board = Board.new()
+    submarine = Ship.new("Submarine", 2)
+    computer = Computer.new()
 
-
-
+    expect(board.valid_placement?(submarine, computer.random_sub_position)).to eq true
+  end
 
 
 
