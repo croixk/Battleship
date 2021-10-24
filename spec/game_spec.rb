63c538c05d328084afe1d20460ede32036b1155b
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-24 14:33:10 -0600

    Add player_board read test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe 'Game' do
     current_game = Game.new()
     expect(current_game).to be_instance_of(Game)
   end
-  
+
+  it 'can read player_board object' do
+    current_game = Game.new()
+    expect(current_game.player_board).to be_instance_of(Board)
+  end
+
 
 
 
